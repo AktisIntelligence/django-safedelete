42c0a39356303d7c4ef6456bcd34ed2002b6b2e2
Thomas Leonard <tleonard@cgscope.co>
2019-08-09 15:43:27 +0100

    Use the fast delete strategy on the collector to not load everything in memory

---
+++ safedelete/collector.py
@@ -0,0 +1,33 @@
+from django.contrib.admin.utils import NestedObjects
+from django.db import router
+
+
+class FastNestedObjects(NestedObjects):
+
+    def can_fast_delete(self, *args, **kwargs):
+        """
+        For some weird reason they hard coded the NestedObjects Collector to not allow fast delete which means that it
+        load all the objects in memory... meaning it is very memory intensive and slow.
+        """
+        return True
+
+
+def get_collector(objs):
+    """
+    Create a collector for the given objects.
+    The collector contains all the objects related to the objects given as input that would need to be modified if we
+    deleted the input objects:
+    - if they need to be deleted by cascade they are in `collector.data` (ordered dictionary) and `collector.nested()`
+    (as a nested list)
+    - if they need to be updated (case of on_delete=models.SET_NULL for example) they are in `collector.field_updates`
+
+    When calling `collector.delete()`, it goes through both types and delete/update the related objects.
+    Note that by doing that it does not call the model delete/save methods.
+
+    Note that `collector.data` also contains the object itself.
+    """
+    # Assume we have at least one object (which is fine since we control where this method is called)
+    collector = FastNestedObjects(using=router.db_for_write(objs[0]))
+    collector.collect(objs)
+    collector.sort()
+    return collector
--- safedelete/utils.py
+++ safedelete/utils.py
@@ -4,9 +4,7 @@ import warnings
 
 from collections import OrderedDict
 
-from django.contrib.admin.utils import NestedObjects
-from django.db import router
-
+from .collector import get_collector
 from .config import DEFAULT_DELETED
 
 logger = logging.getLogger(__name__)
@@ -33,27 +31,6 @@ def is_safedelete(related):
 
 def is_deleted(obj):
     return obj.deleted != DEFAULT_DELETED
-
-
-def get_collector(objs):
-    """
-    Create a collector for the given objects.
-    The collector contains all the objects related to the objects given as input that would need to be modified if we
-    deleted the input objects:
-    - if they need to be deleted by cascade they are in `collector.data` (ordered dictionary) and `collector.nested()`
-    (as a nested list)
-    - if they need to be updated (case of on_delete=models.SET_NULL for example) they are in `collector.field_updates`
-
-    When calling `collector.delete()`, it goes through both types and delete/update the related objects.
-    Note that by doing that it does not call the model delete/save methods.
-
-    Note that `collector.data` also contains the object itself.
-    """
-    # Assume we have at least one object (which is fine since we control where this method is called)
-    collector = NestedObjects(using=router.db_for_write(objs[0]))
-    collector.collect(objs)
-    collector.sort()
-    return collector
 
 
 def get_objects_to_delete(objs, return_deleted=False):
